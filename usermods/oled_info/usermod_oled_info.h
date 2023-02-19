7d75e129b97a825d5c78862713ff9be475d9c66e
Mira Ojamo <1149108+Siikakala@users.noreply.github.com>
2023-02-20 01:10:18 +0200

    if ..kind of works - it just doesn't reset the press counter

--- usermods/oled_info/usermod_oled_info.h
+++ usermods/oled_info/usermod_oled_info.h
@@ -5,6 +5,7 @@
 // #include <Arduino.h>
 
 #define WLED_DEBOUNCE_THRESHOLD 50 // only consider button input of at least 50ms as valid (debouncing)
+#define INFO_TIMEOUT_MS 2000
 
 class UsermodOledInfo : public Usermod
 {
@@ -24,11 +25,12 @@ private:
   unsigned long nextUpdate = 0;
   unsigned long lastRedraw = 0;
   unsigned long overlayUntil = 0;
-  uint8_t buttonPresses = 0;
+  int buttonPresses = 0;
   uint16_t refreshRate = 1000;
   uint32_t screenTimeout = 10000;
 
   String actor = "";
+  uint16_t infoTimeout = INFO_TIMEOUT_MS;
   IPAddress knownIp;
   FourLineDisplayUsermod *display;
   UsermodBattery *battery;
@@ -36,6 +38,7 @@ private:
 
   static const char _name[];
   static const char _enabled[];
+  static const char _infoTimeOut[];
   const char *empty = '\0';
 
   // U8G2 u8g2 = U8G2_SSD1306_128X64_NONAME_2_HW_I2C (U8G2_R0, U8X8_PIN_NONE, HW_PIN_SCL, HW_PIN_SDA);
@@ -84,7 +87,7 @@ public:
     snprintf_P(s1, sizeof(s1), PSTR("Actor:"));
     snprintf_P(s4, sizeof(s4), PSTR("%s"), actor.c_str());
 
-    display->overlay(s1, empty, empty, s4, empty, empty, empty, empty, 5000);
+    display->overlay(s1, empty, empty, s4, empty, empty, empty, empty, infoTimeout);
   }
 
   void showArtNetInfo()
@@ -98,7 +101,7 @@ public:
     // snprintf_P(s5, sizeof(s5), PSTR("Temp: %f C"), temp->getTemperatureC());
     snprintf_P(s6, sizeof(s6), PSTR("IP: %s"), knownIp.toString().c_str());
 
-    display->overlay(s1, cmDNS, empty, empty, empty, s4, s5, s6, 5000);
+    display->overlay(s1, cmDNS, empty, empty, empty, s4, s5, s6, infoTimeout);
   }
 
   bool handleButton(uint8_t b)
@@ -115,50 +118,46 @@ public:
     // momentary button logic
     if (isButtonPressed(b))
     { // pressed
-
       if (!buttonPressedBefore[b])
         buttonPressedTime[b] = now;
       buttonPressedBefore[b] = true;
-      buttonPresses++;
-
-      if (now - buttonPressedTime[b] > 600)
-      {                  // long press
-        handled = false; // use if you want to pass to default behaviour
-        buttonLongPressed[b] = true;
-      }
     }
     else if (!isButtonPressed(b) && buttonPressedBefore[b])
     { // released
-
       long dur = now - buttonPressedTime[b];
       if (dur < WLED_DEBOUNCE_THRESHOLD)
       {
         buttonPressedBefore[b] = false;
         return handled;
-      }                                     // too short "press", debounce
-      bool doublePress = buttonWaitTime[b]; // did we have short press before?
-      buttonWaitTime[b] = 0;
-
-      if (!buttonLongPressed[b])
-      { // short press
-        // if this is second release within 350ms it is a double press (buttonWaitTime!=0)
-        if (doublePress)
-        {
-          handled = false; // use if you want to pass to default behaviour
-        }
-        else
-        {
-          buttonWaitTime[b] = now;
-        }
-      }
+      } // too short "press", debounce
+      buttonWaitTime[b] = now;
       buttonPressedBefore[b] = false;
-      buttonLongPressed[b] = false;
     }
     // if 350ms elapsed since last press/release it is a short press
-    if (buttonWaitTime[b] && now - buttonWaitTime[b] > 350 && !buttonPressedBefore[b])
+    if (buttonWaitTime[b] && now - buttonWaitTime[b] > 350)
     {
+      if (now - buttonWaitTime[b] > infoTimeout)
+      {
+        buttonPresses = 0;
+      }
+      else
+      {
+        buttonPresses++;
+      }
       buttonWaitTime[b] = 0;
-      showActorInfo();
+      switch (buttonPresses)
+      {
+      case 1:
+        showActorInfo();
+        break;
+      case 2:
+        showArtNetInfo();
+        break;
+      default:
+        buttonPresses = 0;
+        display->overlay(empty, empty, empty, empty, empty, empty, empty, empty, 0);
+      }
+      handled = true;
     }
     return handled;
   }
@@ -186,6 +185,7 @@ public:
     JsonObject top = root.createNestedObject(FPSTR(_name));
     top[FPSTR(_enabled)] = enabled;
     top["actor"] = actor;
+    top["infoTimeout"] = infoTimeout;
   }
 
   void appendConfigData()
@@ -194,6 +194,9 @@ public:
     oappend(String(FPSTR(_name)).c_str());
     oappend(SET_F(":actor"));
     oappend(SET_F("',1,'<br /><small><i>Whose control box this is</i></small>');"));
+    oappend(String(FPSTR(_infoTimeOut)).c_str());
+    oappend(SET_F(":infoTimeout"));
+    oappend(SET_F("',2,'<br /><small><i>How long info is displayed after button press</i></small>');"));
   }
 
   bool readFromConfig(JsonObject &root)
@@ -202,6 +205,7 @@ public:
     bool configComplete = !top.isNull();
     configComplete &= getJsonValue(top[FPSTR(_enabled)], enabled);
     configComplete &= getJsonValue(top["actor"], actor);
+    configComplete &= getJsonValue(top["infoTimeout"], infoTimeout);
     return configComplete;
   }
 
@@ -213,3 +217,4 @@ public:
 
 const char UsermodOledInfo::_name[] PROGMEM = "OledInfo";
 const char UsermodOledInfo::_enabled[] PROGMEM = "enabled";
+const char UsermodOledInfo::_infoTimeOut[] PROGMEM = "infoTimeOutms";
